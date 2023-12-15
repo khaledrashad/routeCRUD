9ef6ee60ba3a726bd57386b2f995b42a7113804a
amr rashad <khaledamr400@gmail.com>
2023-12-15 11:44:16 +0200

    updateProduct function added

--- js/main.js
+++ js/main.js
@@ -4,15 +4,12 @@ var categoryInput = document.getElementById("productCategory")
 var saleInput = document.getElementById("productSale")
 var descriptionInput = document.getElementById("productDescription")
 var searchInput = document.getElementById("searchInput")
-var updateBtn = document.getElementById("#updateButton")
 
 var productList = []
 if (localStorage.getItem("storageData") != null) {
     productList = JSON.parse(localStorage.getItem("storageData"))
     displayProduct()
 }
-
-
 function addProduct() {
     var product = {
         name: nameInput.value,
@@ -37,14 +34,17 @@ function displayProduct() {
         <td>`+ productList[i].sale + `</td>
         <td>`+ productList[i].desc + `</td>
         <td>
-            <button onclick="updateProduct()"  id="updateButton" class="btn btn-warning">Update</button>
+            <button onclick="updateProduct(`+ i + `)"  id="updateButton" class="btn btn-warning">Update</button>
         </td>
         <td>
-            <button onclick="removeProduct(`+ i +`)" class="btn btn-danger">Delete</button>
+            <button onclick="removeProduct(`+ i + `)" class="btn btn-danger">Delete</button>
         </td>
     </tr>`
     }
-    document.getElementById("myTable").innerHTML = temp
+    document.getElementById("myTable").innerHTML = temp;
+
+
+
 }
 function searchProduct() {
     var searchVal = searchInput.value.toLowerCase()
@@ -69,20 +69,39 @@ function searchProduct() {
         document.getElementById("myTable").innerHTML = result
     }
 }
-function removeProduct(index){
-    productList.splice(index,1)
+function removeProduct(index) {
+    productList.splice(index, 1)
     displayProduct()
     localStorage.setItem("storageData", JSON.stringify(productList))
 }
-function clearForm(){
+function clearForm() {
     nameInput.value = ""
     priceInput.value = ""
     categoryInput.value = ""
     saleInput.value = false
     descriptionInput.value = ""
 }
-function updateProduct(){
-    var update = updateBtn.addEventListener('click',function(e){
-        console.log(update.e)
+function updateProduct(updateIndex) {
+    nameInput.value = productList[updateIndex].name
+    priceInput.value = productList[updateIndex].price
+    categoryInput.value = productList[updateIndex].cat
+    saleInput.value = productList[updateIndex].sale
+    descriptionInput.value = productList[updateIndex].desc
+    var add_btn = document.getElementById('addBtn')
+    var edit_btn = document.getElementById('editBtn')
+    add_btn.classList.add("d-none")
+    edit_btn.classList.remove("d-none")
+    edit_btn.addEventListener("click", function () {
+        var updatedItem = {
+            name: nameInput.value,
+            price: priceInput.value,
+            cat: categoryInput.value,
+            sale: saleInput.checked,
+            desc: descriptionInput.value
+        }
+        productList.splice(updateIndex, 1, updatedItem)
+        console.log(updatedItem)
+        localStorage.setItem("storageData", JSON.stringify(productList))
+        displayProduct()
     })
 }
